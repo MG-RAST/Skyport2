b2003810c677fb4edfd9668ef7101eccbc6cfb58
Andreas Wilke <wilke@mcs.anl.gov>
2017-10-04 13:45:06 -0500

    Enabled auth and upload

--- Services/ShockBrowser/js/config.js
+++ Services/ShockBrowser/js/config.js
@@ -9,5 +9,6 @@ var RetinaConfig = {
     "awe_url": "http://localhost:8001/awe",
     "mgrast_api": "http://api.metagenomics.anl.gov",
     "shock_url": "http://localhost:8001/shock/api",
-    "authentication": true
+    "authentication": true ,
+    "enableUpload": true
 };
