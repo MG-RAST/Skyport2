037b913ffdefb8ba855cfdbf1ad6c66c8d59a7a6
Andreas Wilke <wilke@mcs.anl.gov>
2017-10-16 15:43:30 -0500

    Changed auth url

--- Config/ShockBrowser/config.js
+++ Config/ShockBrowser/config.js
@@ -1,5 +1,6 @@
 var RetinaConfig = {
     "awe_url": "http://localhost:8001/awe",
     "shock_url": "http://localhost:8001/shock/api",
-    "auth_url": "http://localhost:8001/auth"
+    "auth_url": "http://localhost:8001/auth/cgi-bin/"
+    // "auth_url": "http://localhost:8001/skyport/login/"
 };
