e7eaf1949015eaec600704a319e001a484e634c5
Folker Meyer <folker@anl.gov>
2018-01-19 16:10:01 -0600

    added version for MacOS

--- init-client.sh
+++ init-client.sh
@@ -13,7 +13,12 @@ fi
   
 
 # setting this to this machines local IP address; for production use, set this to the IP address of your SKYPORT server
-export SKYPORT_HOST=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)`
+MYIP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)`
+
+# for MacOS use 
+# MYIP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+
+export SKYPORT_HOST=${MYIP}
 
 export SKYPORT_TMPDIR=`pwd`/tmp
 mkdir -p ${SKYPORT_TMPDIR}
