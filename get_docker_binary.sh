860a906cfda7c28fc28e84ac3acc98d139155c5f
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-01-19 14:24:07 -0600

    new script to get docker binary

---
+++ get_docker_binary.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+if [ -z "${SKYPORT_TMPDIR}" ]; then
+  SKYPORT_TMPDIR="/tmp/"
+fi
+
+if [ -z "${DATADIR}" ]; then
+  DATADIR=`pwd`
+fi
+
+
+export DOCKER_VERSION=$(docker --version | grep -o "[0-9]*\.[0-9]*\.[0-9a-z\.-]*")
+export DOCKER_BINARY=${DATADIR}/docker-${DOCKER_VERSION}
+
+
+if [ ! -e ${DOCKER_BINARY} ] ; then
+  curl -fsSL -o ${SKYPORT_TMPDIR}/docker-${DOCKER_VERSION}.tgz https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz 
+  if [ ! -e ${SKYPORT_TMPDIR}/docker-${DOCKER_VERSION}.tgz ] ; then
+    echo "download.docker.com did not work, try old location get.docker.com..."
+    curl -fsSL -o ${SKYPORT_TMPDIR}/docker-${DOCKER_VERSION}.tgz  https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}.tgz
+  fi
+  
+  if [ ! -e ${SKYPORT_TMPDIR}/docker-${DOCKER_VERSION}.tgz ] ; then
+    echo "docker binary not found"
+    exit 1
+  fi
+  
+  tar -xvzf ${SKYPORT_TMPDIR}/docker-${DOCKER_VERSION}.tgz -C ${SKYPORT_TMPDIR} docker/docker 
+  mv ${SKYPORT_TMPDIR}/docker/docker ${DATADIR}/docker-${DOCKER_VERSION}
+  
+  
+fi
+
+echo "DOCKER_BINARY=${DOCKER_BINARY}"
--- init.sh
+++ init.sh
@@ -13,11 +13,10 @@ fi
   
 
 
-# Top level data dir (using name DATADIR as DATA was conflicting with docker)
-export DATADIR=`pwd`/skyportdata
+# Top level data dir
+export DATADIR=`pwd`/live-data
 mkdir -p $DATADIR
 
-# this is being used for CWL_Runner to download etc.
 export SKYPORT_TMPDIR=$DATADIR/tmp
 mkdir -p ${SKYPORT_TMPDIR}
 
@@ -26,20 +25,12 @@ mkdir -p ${SKYPORT_TMPDIR}
 export SHOCKDIR=${DATADIR}/shock/
 mkdir -p ${SHOCKDIR}/data
 mkdir -p ${SHOCKDIR}/log
-mkdir -p ${SHOCKDIR}/db
 
-# Path to AWE-server
-export AWEDIR=${DATADIR}/awe-server
-mkdir -p ${AWEDIR}
-mkdir -p ${AWEDIR}/db
-mkdir -p ${AWEDIR}/logs
-
-# Path for AWE-worker
+# Path to AWE
+export AWEDIR=${DATADIR}/awe
+mkdir -p ${DATADIR}/awe
+mkdir -p ${DATADIR}/awe/db
 mkdir -p ${DATADIR}/awe-worker/work
-
-# Path for MySQL DB
-export AUTHDB=${DATADIR}/authdb
-mkdir -p ${AUTHDB}
 
 
 # Path to primary log dir
@@ -60,36 +51,18 @@ export TAG=demo
 
 
 
-DOCKER_VERSION=$(docker --version | grep -o "[0-9]*\.[0-9]*\.[0-9a-z\.-]*")
-export DOCKER_BINARY=${DATADIR}/docker-${DOCKER_VERSION}
-
-
-if [ ! -e ${DOCKER_BINARY} ] ; then
-  curl -fsSL -o ${SKYPORT_TMPDIR}/docker-${DOCKER_VERSION}.tgz https://download.docker.com/linux/static/stable/x86_64/docker-${DOCKER_VERSION}.tgz 
-  if [ ! -e ${SKYPORT_TMPDIR}/docker-${DOCKER_VERSION}.tgz ] ; then
-    echo "download.docker.com did not work, try old location get.docker.com..."
-    curl -fsSL -o ${SKYPORT_TMPDIR}/docker-${DOCKER_VERSION}.tgz  https://get.docker.com/builds/Linux/x86_64/docker-${DOCKER_VERSION}.tgz
-  fi
-  
-  if [ ! -e ${SKYPORT_TMPDIR}/docker-${DOCKER_VERSION}.tgz ] ; then
-    echo "docker binary not found"
-    exit 1
-  fi
-  
-  tar -xvzf ${SKYPORT_TMPDIR}/docker-${DOCKER_VERSION}.tgz -C ${SKYPORT_TMPDIR} docker/docker 
-  mv ${SKYPORT_TMPDIR}/docker/docker ${DATADIR}/docker-${DOCKER_VERSION}
-  
-  
-fi
+source ./get_docker_binary.sh
 
 
 echo Set config to:
-echo TAG=$TAG 
-echo CONFIGDIR=$CONFIGDIR
-echo SHOCKDIR=${SHOCKDIR}
-echo DATADIR=${DATADIR}
-echo LOGDIR=${LOGDIR}
+echo "TAG=${TAG}"
+echo "CONFIGDIR=${CONFIGDIR}"
+echo "SHOCKDIR=${SHOCKDIR}"
+echo "DATADIR=${DATADIR}"
+echo "LOGDIR=${LOGDIR}"
 echo "DOCKER_VERSION=${DOCKER_VERSION}"
+echo "DOCKER_BINARY=${DOCKER_BINARY}"
+
 
 echo ""
 echo "Next step: docker-compose -f Docker/Compose/skyport-demo.yaml up"
