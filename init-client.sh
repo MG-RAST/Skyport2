3416eaea863a3723039debba519fbfe0ccde180e
Folker Meyer <folker@anl.gov>
2018-01-19 16:29:33 -0600

    fixed and tested

--- init-client.sh
+++ init-client.sh
@@ -5,18 +5,22 @@
 ############################
 
 
-
 if [[ $_ == $0 ]]; then 
   echo "Error: please use command \"source ./init-client.sh\""
   exit 1
 fi
   
+OS=`uname -s`
 
 # setting this to this machines local IP address; for production use, set this to the IP address of your SKYPORT server
-MYIP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)`
+# 
+if [ ${OS} == "Darwin" ]
+then
+  MYIP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
+else
+  MYIP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
+fi
 
-# for MacOS use 
-# MYIP=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 
 export SKYPORT_HOST=${MYIP}
 
