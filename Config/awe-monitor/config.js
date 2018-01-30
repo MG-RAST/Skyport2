74da3b38516a724580695a428c14d81628a2a704
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-01-30 17:21:38 -0600

    added stand-alone awe-monitor

---
+++ Config/awe-monitor/config.js
@@ -0,0 +1,18 @@
+var RetinaConfig = {
+    "awe_ip": "http://localhost:8001/awe/api/",
+    "workflow_ip": "http://localhost:8001/awe/api/awf",
+    "authResources": { 
+        "default": "OAUTH",
+        "OAUTH": {
+            "icon":"OAUTH_favicon.ico",
+            "prefix":"oago4711",
+            "keyword":"auth",
+            "url":"",
+            "useHeader":false,
+            "bearer":"oauth"} 
+        },
+    "mgrast_api": "http://api.metagenomics.anl.gov",
+    "authentication": true,
+    "useOAuthServer": true,
+    "auth_url": "http://localhost:8001/auth/cgi-bin"
+}
