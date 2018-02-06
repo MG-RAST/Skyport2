dee2109f48ad1fed0efc0afa9edf9ddc87a18ddf
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-02-06 13:42:50 -0600

    add trailing slash

--- Config/ShockBrowser/config.js
+++ Config/ShockBrowser/config.js
@@ -1,5 +1,5 @@
 var RetinaConfig = {
-    "awe_url": "http://localhost:8001/awe",
+    "awe_url": "http://localhost:8001/awe/",
     "shock_url": "http://localhost:8001/shock/api",
     "auth_url": "http://localhost:8001/auth/cgi-bin/"
     // "auth_url": "http://localhost:8001/skyport/login/"
