bf1efebfefa53da1f262f72cc30964d5a327186f
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-02-20 09:28:08 -0600

    better error message

--- get_ip_address.sh
+++ get_ip_address.sh
@@ -9,8 +9,10 @@ if [[ $_ == $0 ]]; then
   exit 1
 fi
 
-if [ ! -z ${SKYPORT_HOST} ] ; then
-  echo "SKYPORT_HOST already configured: SKYPORT_HOST=${SKYPORT_HOST}"
+if [ ! -z ${USE_SKYPORT_HOST} ] ; then
+  SKYPORT_HOST=${USE_SKYPORT_HOST}
+  echo "Variable USE_SKYPORT_HOST has been set."
+  echo "SKYPORT_HOST=${SKYPORT_HOST}"
   return 0
 fi
 
@@ -20,16 +22,14 @@ fi
 MY_IP=$(for i in $(ifconfig -a | cut -d ' ' -f 1 | cut -d $'\t' -f 1 | grep -Ev "^$" | grep -v "^veth\|^lo\|^docker\|^br" | cut -d : -f 1) ; do ifconfig $i ; done | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')  
 
 COUNT=$(echo ${MYIP} | wc -l )
-if [ ${COUNT} -eq 0 ] ; then
-  echo "no ip address found"
-  return 1
-fi 
 
-if [ ${COUNT} -gt 1 ] ; then
+if [ ${COUNT} -gt 1 ] || [ ${COUNT} -eq 0 ] ; then
   echo ""
   echo ${MYIP}
   echo ""
-  echo "Auto-detection of IP address failed: More than one IP address found. Please specify one manually: export SKYPORT_HOST=<your IP address>"
+  echo "Auto-detection of IP address failed: Please specify one manually:"
+  echo "> export USE_SKYPORT_HOST=<your IP address>"
+  echo "Then execute \"source ./init.sh\""
   return 1
 fi 
 
