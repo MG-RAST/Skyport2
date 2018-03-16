d59fa675c50f0f9a276724b7c9f8e8f05671ec81
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-03-16 10:52:11 -0500

    add user-friendly overview script

---
+++ skyport2-overview.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo
+echo "------- Skyport2 -----------------------------"
+
+echo "Skyport2 main URL: ${SKYPORT_URL}"
+echo 
+echo "AWE monitor:       ${SKYPORT_URL}/awe/api/"
+echo
+echo "AWE server API:    ${AWE_SERVER_URL}"
+echo "Shock server API:  ${SHOCK_SERVER_URL}"
+echo
+echo "Auth server:       ${AUTH_URL}"
+echo
+echo "----------------------------------------------"
+echo 
