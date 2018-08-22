78d43ac9d9151d1be81877feeffc58e7c46dfa42
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2018-08-22 10:51:33 -0500

    new wrapper

---
+++ start-skyport.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# with commando "clean" all containers with name prefix skyport2_ will be killed
+
+if [ "${1}x" == "cleanx" ] ; then
+  docker rm -f $(docker ps -a -f name=skyport2_ -q)
+  sleep 2
+fi
+
+set -x
+set -e
+
+
+source ./init.sh
+exec docker-compose up
